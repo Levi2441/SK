8d5e49390041c7a6f171b14edb90585ebb43438d
Levi Zeng <lezeng003006@gmail.com>
2024-01-02 21:19:03 -0500

    Working version 1

--- Front-End/src/services/data.js
+++ Front-End/src/services/data.js
@@ -1,15 +1,15 @@
 import axios from "axios";
 
 const getAllProducts = () => {
-  return axios.get("/api/products");
+  return axios.get("http://localhost:3001/api/products");
 };
 
 const getAllIngredients = () => {
-  return axios.get("/api/ingredients");
+  return axios.get("http://localhost:3001/api/ingredients");
 };
 
 const addProduct = (new_product) => {
-  return axios.post("/api/suggestions", new_product);
+  return axios.post("http://localhost:3001/api/suggestions", new_product);
 };
 
 export default {
