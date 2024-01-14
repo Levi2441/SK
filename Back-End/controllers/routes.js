35d121215b61a82dae377cd50fbb801599772f99
Levi Zeng <lezeng003006@gmail.com>
2024-01-14 18:26:49 -0500

    Organized files into directories

---
+++ Back-End/controllers/routes.js
@@ -0,0 +1,101 @@
+const router = require("express").Router();
+
+const Ing = require("../mongo/ing");
+const Product = require("../mongo/product");
+const Suggestion = require("../mongo/sug");
+
+router.get("/", (request, response) => {
+  response.status(200).send("<p>Server Up</p>");
+});
+
+router.get("/api/ingredients", (request, response) => {
+  Ing.find({})
+    .then((res) => {
+      //console.log("success");
+      response.status(200).json(res);
+    })
+    .catch(() => {
+      console.log("error");
+    });
+});
+
+router.get("/api/products", (request, response) => {
+  Product.find({})
+    .then((res) => {
+      //console.log("success");
+      response.status(200).json(res);
+    })
+    .catch(() => {
+      console.log("error");
+    });
+});
+/**
+ * Set up routes to get a specific product by id
+ */
+router.get("/api/products/:id", (request, response) => {
+  Product.findById(request.params.id)
+    .then((res) => {
+      //console.log("success");
+      response.status(200).json(res);
+    })
+    .catch(() => {
+      console.log("error getting product");
+    });
+});
+
+router.get("/api/ingredients/:id", (request, response) => {
+  Ing.findById(request.params.id)
+    .then((res) => {
+      //console.log("success");
+      response.status(200).json(res);
+    })
+    .catch(() => {
+      console.log("error getting ingredient");
+    });
+});
+
+/*
+ *Set up route for adding/deleting a suggestion
+ *Should just contain information about name and brand of product
+ */
+
+router.post("/api/suggestions", (request, response) => {
+  //use Sug model to create a new object
+  //request.body should contain the object
+  let sug_data = request.body;
+
+  //build the new suggestion
+  let new_sug = Suggestion({
+    name: sug_data.name,
+    brand: sug_data.brand,
+  });
+
+  //invoke .save() method
+
+  new_sug
+    .save()
+    .then((res) => {
+      response.status(200).json(res);
+    })
+    .catch((error) => {
+      console.log("Error adding suggestion");
+    });
+});
+
+router.delete("/api/suggestions/:id", (request, response) => {
+  Suggestion.findByIdAndDelete(request.params.id).then((deletedObj) => {
+    if (deletedObj) {
+      response.status(200).send("<p>Successfully Deleted</p>");
+    } else {
+      response.status(404).send("<p>Product not found</p>");
+    }
+  });
+});
+//also set up a route to view
+router.get("/api/suggestions", (request, response) => {
+  Suggestion.find({}).then((res) => {
+    response.status(200).json(res);
+  });
+});
+
+module.exports = router;
