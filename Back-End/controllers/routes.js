6d91dc2e6b6e0b341a779facac937c00781e4b7c
Levi Zeng <lezeng003006@gmail.com>
2024-02-18 13:57:04 -0500

    Suggestion feature updates: -got two server approach to run -still have to test and check for all cases of missing information

--- Back-End/controllers/routes.js
+++ Back-End/controllers/routes.js
@@ -84,12 +84,77 @@ router.post("/api/suggestions", (request, response) => {
   //   .catch((error) => {
   //     console.log("Error adding suggestion");
   //   });
-  console.log(query);
+  //console.log(query);
   axios
     .post("http://localhost:3002/product", query)
     .then((res) => {
-      //console.log(res);
-      response.status(200).send(res.data);
+      //either the all the informatoin or an object with name set to "No Objects found"
+      let product = res.data;
+
+      //first check that the query result is valid
+      if (product.name == "No Products Found") {
+        response.status(200).send("No Products Found");
+      } else {
+        //have to check if its not already in the database
+        //check by name -- deal with brand later?
+        // console.log(product.product_name);
+        Product.findOne({ name: product.product_name }).then(function (item) {
+          //checks for item in databse
+          if (item != null) {
+            response.status(200).send("Item already in Database");
+          } else {
+            //after run a check on all the items -- ingredients should be safe
+            /**
+             * product_name should be there
+             * product_brand does not have to be there
+             * product_category also does not have to be there
+             * overall_score also does not have to be there
+             * we have to account for these errors
+             * the only thing that necessary to our calculations is score (brand and category can be none)
+             */
+            //console.log(product);
+
+            if (product.overall_score > -1 && product.brand != "") {
+              //ship it off to the databse
+
+              //handle brand and category
+              let product_brand = "";
+              let product_category = "";
+
+              if (product.product_brand != "") {
+                product_brand = product.product_brand;
+              }
+              if (product.product_category != "") {
+                product_category = product.product_category;
+              }
+
+              let product_object = Product({
+                ingredients: product.product_ingredients,
+                name: product.product_name,
+                rating: product.overall_score,
+                brand: product_brand,
+                category: product_category,
+              });
+
+              product_object
+                .save()
+                .then((res) => {
+                  response
+                    .status(200)
+                    .send("Product Added to Database Successfully");
+                })
+                .catch((err) => {
+                  console.log(err);
+                });
+            } else {
+              //Key bits of information were not found
+              response
+                .status(200)
+                .send("Key bits of information were not found");
+            }
+          }
+        });
+      }
     })
     .catch((err) => {
       console.log(err);
--- Front-End/src/services/data.js
+++ Front-End/src/services/data.js
@@ -1,15 +1,17 @@
 import axios from "axios";
 
+const base_url = "http://localhost:3001";
+
 const getAllProducts = () => {
-  return axios.get("/api/products");
+  return axios.get(base_url + "/api/products");
 };
 
 const getAllIngredients = () => {
-  return axios.get("/api/ingredients");
+  return axios.get(base_url + "/api/ingredients");
 };
 
 const addProduct = (new_product) => {
-  return axios.post("/api/suggestions", new_product);
+  return axios.post(base_url + "/api/suggestions", new_product);
 };
 
 export default {
